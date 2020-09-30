41fd1ccf6fd27f938723dc3cb6a22c42ce002a08
KaviselviG <KaviselviG@softura.com>
2020-09-30 22:48:07 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import App from './App';
 
 
-import { SteppedLineTo } from 'react-lineto';
-import LineTo from 'react-lineto';
-import { Line } from 'react-lineto';
+// import { SteppedLineTo } from 'react-lineto';
+// import LineTo from 'react-lineto';
+// import { Line } from 'react-lineto';
 
 ReactDOM.render(
   <>
     <App />
-    <div className="A">Element A</div>
+    {/* <div className="A">Element A</div>
             <div className="B">Element B</div>
             <LineTo from="A" to="B" />
             <SteppedLineTo from="A" to="B" orientation="v" />
-            <Line x0={0} y0={0} x1={10} y1={10} />     
+            <Line x0={0} y0={0} x1={10} y1={10} />      */}
   </>,
   document.getElementById('root')
 );
